ef870e2b1a7fa4248bd3c6cc1757ffaab042dce9
gustavo <gvmarvelos@gmail.com>
2019-07-25 09:01:26 -0300

    chore: Inclusão e remoção da lista

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
 import "./App.css";
-import profile from "./asserts/profile.jpg";
+
+import TechList from "./components/TechList";
 
 function App() {
-  return <img width="200" src={profile} />;
+  return <TechList />;
 }
 
 export default App;
